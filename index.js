44429b3f57212d69e9354bf16e7b767a0cab0ae0
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-03-25 18:54:41 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(array){
+    const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+        //...
+      ]
+      const winningRecord = array.find(record => record.result === "W");
+  
+      if (winningRecord) {
+        return winningRecord.year;
+      }
+    else {
+      return undefined;
+    }
+}
